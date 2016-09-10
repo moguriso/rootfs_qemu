c6f3fae1fce8651e689e8b7113a2834366636367
Yuya Adachi <moguriso@tizen.moe>
2016-09-10 16:09:45 +0900

    add some qemu run option

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-qemu-system-arm -M versatilepb -kernel 05-images/zImage -m 128M -initrd 05-images/initramfs.gz -append "root=/dev/ram rdinit=/bin/sh" $@
+# qemu-system-arm -M versatilepb -serial stdio -nographic -kernel 05-images/zImage -m 128M -initrd 05-images/initramfs.gz -append "root=/dev/ram rdinit=/bin/sh console=ttyS0 nosplash" $@
+# qemu-system-arm -M realview-pbx-a9 -serial stdio -nographic -kernel 05-images/zImage -m 128M -initrd 05-images/initramfs.gz -append "root=/dev/ram rdinit=/bin/sh console=ttyS0 nosplash" $@
+# qemu-system-arm -M versatilepb -nographic -kernel 05-images/zImage -m 128M -initrd 05-images/initramfs.gz -append "root=/dev/ram rdinit=/bin/sh console=ttyAMA0" $@
+
+# qemu-system-arm -M versatilepb -kernel 05-images/zImage -m 128M -initrd 05-images/initramfs.gz -append "root=/dev/ram rdinit=/bin/sh" $@
+
+qemu-system-arm -M versatilepb -nographic -kernel 05-images/zImage -m 128M -initrd 05-images/initramfs.gz -append "root=/dev/ram rdinit=/bin/sh console=ttyAMA0" $@
